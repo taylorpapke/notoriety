a4f09411a0b4df6d9d810c27bafe5f612ec1d535
Jason Panzer <jason.drew.panzer@gmail.com>
2021-11-01 18:40:24 -0700

    add notecard layout

--- client/src/components/Button/Button.jsx
+++ client/src/components/Button/Button.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import './Button.css'
+import React from "react"
+import PropTypes from "prop-types"
+import "./Button.css"
 
 const Button = ({ buttonStyle, children, onClickHandler }) => {
   return (
     <div className="button-container">
-      <button className={`button-common ${buttonStyle}`} onClick={onClickHandler}>{children}</button>
+      <button
+        className={`button-common ${buttonStyle}`}
+        onClick={onClickHandler}
+      >
+        {children}
+      </button>
     </div>
   )
 }
 
 Button.propTypes = {
-  buttonStyle: PropTypes.oneOf(['primary', 'secondary']).isRequired,
-  onClickHandler: PropTypes.func.isRequired
+  buttonStyle: PropTypes.oneOf(["primary", "secondary"]).isRequired,
+  onClickHandler: PropTypes.func.isRequired,
 }
 
 export default Button
--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import Button from '../Button/Button';
-import './Card.css';
+import React from "react"
+import Button from "../Button/Button"
+import "./Card.css"
 
 const Card = () => {
   return (
     <div className="flashcard-wrapper">
       <div className="card-surface">
-        <h3>Card Content</h3>
+        <div className="card-content">
+          <h3>Content</h3>
+        </div>
       </div>
       <Button buttonStyle="primary">Stuff</Button>
     </div>
-  );
-};
+  )
+}
 
 export default Card
