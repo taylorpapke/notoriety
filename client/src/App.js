eef53f8ec100ffc42d0127753c418771a3013acf
Jason Panzer <jason.drew.panzer@gmail.com>
2021-11-07 18:21:04 -0800

    Adds create card form

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react'
-import CardProvider from './components/CardProvider/CardProvider'
+// import CardProvider from './components/CardProvider/CardProvider'
 import Container from './components/Container/Container'
 import TopBar from './components/TopBar/TopBar'
 import './App.css'
+import CreateCard from './components/CreateCard/CreateCard'
 
 const App = () => {
   return (
     <div className="wrapper">
       <TopBar />
       <Container>
-        <CardProvider />
+        <CreateCard />
       </Container>
     </div>
   )
--- client/src/CreateCard/CreateCard.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const CreateCard = () => {
-  return <div>Create Card!</div>
-}
-
-export default CreateCard
