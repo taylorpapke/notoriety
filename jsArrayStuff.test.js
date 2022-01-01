288daf0ab46ffc9f1066daa67fc8740f37fa6cbc
Jason Panzer <jason.drew.panzer@gmail.com>
2022-01-01 15:31:04 -0800

    adding tests for array methods

---
+++ jsArrayStuff.test.js
@@ -0,0 +1,27 @@
+const { interestingObject, bindingObjectExample, makeAQueue } = require('./jsArrayStuff');
+
+describe('Array methods', () => {
+  const expectedFilterExample = [3, 5, 8, 9];
+  const expectedMapExample = ['3', '5', '8', 'a', '[object Object]', 'true', '9'];
+  it('should behave in the expected manner', () => {
+    expect(interestingObject.filterExample()).toEqual(expectedFilterExample);
+    expect(interestingObject.mapExample()).toEqual(expectedMapExample);
+    expect(interestingObject.reduceExample()).toEqual(25)
+  });
+});
+
+describe('Arrays like queues', () => {
+  const aQueue = makeAQueue()
+  let result
+
+  beforeEach(() => {
+    result = []
+    result.push(aQueue.shift())
+    result.push(aQueue.shift())
+    result.push(aQueue.shift())
+  });
+
+  it('should ', () => {
+    
+  });
+});
