4cf2cd95511dbddd5c16c3461e01fdbe9e2ec68d
Jason Panzer <jason.drew.panzer@gmail.com>
2022-01-06 20:41:06 -0800

    changing word in div

--- button.js
+++ button.js
@@ -1,27 +1,24 @@
 function changeColor() {
-  var blueDiv = document.getElementById("changeable");
+  var blueDiv = document.getElementById('changeable');
   // Use the Mozilla Developer Network resource to find the right way to change
-  // the background for this div 
-  blueDiv.setAttribute('class', 'purple half')
+  // the background for this div
+  blueDiv.setAttribute('class', 'purple half');
 }
 
-function replaceWord() {
+async function replaceWord() {
   var wordable = document.getElementById('wordable');
 
-  fetch("https://wordsapiv1.p.rapidapi.com/words/?random=true", {
-    method: "GET",
+  fetch('https://wordsapiv1.p.rapidapi.com/words/?random=true', {
+    method: 'GET',
     headers: {
-      "x-rapidapi-key": "your-api-key-here",
-      "x-rapidapi-host": "api-host-here",
+      'x-rapidapi-host': 'wordsapiv1.p.rapidapi.com',
+      'x-rapidapi-key': 'keys',
     },
   })
     .then((response) => {
-      console.log(response);
-      response.json().then(() => {
-        /* 
-          Complete this handler function with code that populates an html 
-          element with the random word 
-        */
+      response.json().then((responseBody) => {
+        console.log(responseBody.word);
+        wordable.textContent = responseBody.word
       });
     })
     .catch((err) => {
